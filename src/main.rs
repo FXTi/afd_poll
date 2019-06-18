04b1d9735c31d775908512521958e17ca980fcd1
FXTi <zjxiang1998@gmail.com>
2019-06-18 15:34:44 +0800

    Add ws_get_base_socket

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use miow::Overlapped;
 use ntapi::ntioapi::{IO_STATUS_BLOCK_u, NtDeviceIoControlFile, IO_STATUS_BLOCK};
 use std::mem::size_of;
-use winapi::shared::minwindef::ULONG;
+use winapi::shared::minwindef::{DWORD, ULONG};
 use winapi::shared::ntdef::{NTSTATUS, PVOID};
 use winapi::um::winnt::{HANDLE, LARGE_INTEGER};
+use winapi::um::winsock2::{WSAIoctl, SOCKET};
 
 #[repr(C)]
 struct AFD_POLL_HANDLE_INFO {
@@ -20,7 +21,11 @@ struct AFD_POLL_INFO {
     Handles: [AFD_POLL_HANDLE_INFO; 1],
 }
 
-fn afd_poll(afd_helper_handle: HANDLE, poll_info: &mut AFD_POLL_INFO, overlapped: &Overlapped) -> i32 {
+fn afd_poll(
+    afd_helper_handle: HANDLE,
+    poll_info: &mut AFD_POLL_INFO,
+    overlapped: &Overlapped,
+) -> i32 {
     let mut iosb = IO_STATUS_BLOCK {
         u: IO_STATUS_BLOCK_u {
             Status: winapi::shared::ntstatus::STATUS_PENDING,
@@ -44,6 +49,25 @@ fn afd_poll(afd_helper_handle: HANDLE, poll_info: &mut AFD_POLL_INFO, overlapped
     }
 
     0
+}
+
+fn ws_get_base_socket(socket: &SOCKET) -> SOCKET {
+    let mut base_socket: SOCKET = 0;
+    let mut bytes: DWORD = 0;
+
+    unsafe {
+        WSAIoctl(
+            socket,
+            0x48000022,
+            0 as *mut _,
+            0,
+            &mut base_socket as *mut _,
+            size_of::<SOCKET>() as DWORD,
+            &mut bytes as *mut _,
+            0 as *mut _,
+            None,
+        );
+    }
 }
 
 fn main() {
