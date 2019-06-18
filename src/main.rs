4700758a741811e7889b18e0c5207a02f2491ddf
FXTi <zjxiang1998@gmail.com>
2019-06-18 15:37:44 +0800

    test

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use miow::Overlapped;
 use ntapi::ntioapi::{IO_STATUS_BLOCK_u, NtDeviceIoControlFile, IO_STATUS_BLOCK};
 use std::mem::size_of;
-use winapi::shared::minwindef::{DWORD, ULONG};
+use winapi::shared::minwindef::{DWORD, ULONG, LPVOID};
 use winapi::shared::ntdef::{NTSTATUS, PVOID};
 use winapi::um::winnt::{HANDLE, LARGE_INTEGER};
 use winapi::um::winsock2::{WSAIoctl, SOCKET};
@@ -57,17 +57,19 @@ fn ws_get_base_socket(socket: &SOCKET) -> SOCKET {
 
     unsafe {
         WSAIoctl(
-            socket,
+            *socket,
             0x48000022,
             0 as *mut _,
             0,
-            &mut base_socket as *mut _,
+            &mut base_socket as *mut _ as LPVOID,
             size_of::<SOCKET>() as DWORD,
             &mut bytes as *mut _,
             0 as *mut _,
             None,
         );
     }
+
+    base_socket
 }
 
 fn main() {
