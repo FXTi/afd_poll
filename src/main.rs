a780890e353d1b53141a7b78e8f4f17d5574aaf3
FXTi <zjxiang1998@gmail.com>
2019-06-20 22:00:19 +0800

    emmm.... proc_macro import issue?

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #[macro_use]
 extern crate lazy_static;
-#![feature(proc_macro, proc_macro_non_items)]
-extern crate wchar;
+#![feature(proc_macro_hygiene)]
+use wchar::wch_c;
 use ntapi::ntioapi::{
     IO_STATUS_BLOCK_u, NtCreateFile, NtDeviceIoControlFile, FILE_OPEN, IO_STATUS_BLOCK,
 };
